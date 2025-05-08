c61cb24015c395882c94824447ad8cac2558ee2d
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-09 01:39:39 +0700

    menambahkan checkbox untuk mengubah status todo menjadi selesai atau belum selesai

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -145,21 +145,29 @@ class TodoPage extends StatelessWidget {
                                             : Colors.red,
                                       ),
                                     ),
-
-                                  ]
-                                )
-                              ]
-                            )
-                          )
-                          }
-                      )
-
+                                  ],
+                                ),
+                                Checkbox(
+                                  value: todo.isCompleted,
+                                  onChanged: (value) {
+                                    context.read<TodoBloc>().add(
+                                          TodoEventComplete(index: index),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return Center(child: Text('No todos available'));
                     }
                   }
                 )
-                )
-              ],
-            ),
+              )
+            ],
+          ),
         ),
       ),
     );
