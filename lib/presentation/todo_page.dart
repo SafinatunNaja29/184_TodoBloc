5553ccdc66ff2b32992233a4d7a8867f5d45a4cc
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-09 01:02:49 +0700

    nambahkan struktur dasar halaman dengan Scaffold dan SafeArea

---
+++ lib/presentation/todo_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:materi_bloc2/todo_bloc/todo_bloc.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: []
+            )
+        )
+      )
+    );
+  } 
+}
